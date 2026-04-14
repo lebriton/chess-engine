0898af65219dfc8822f848203ae994a74f0dbd05
Bryton Lacquement <hello@brytonlacquement.com>
2026-04-14 18:12:39 +0200

    Use num_enum

--- src/core/piece.rs
+++ src/core/piece.rs
@@ -1,4 +1,7 @@
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+use num_enum::TryFromPrimitive;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, TryFromPrimitive)]
+#[repr(u8)]
 pub enum Piece {
     Pawn,
     Knight,
