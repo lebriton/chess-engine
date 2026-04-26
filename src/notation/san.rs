24a1126e2c5780d5b4700ba32409c8d973a47328
Bryton Lacquement <hello@brytonlacquement.com>
2026-04-26 17:10:55 +0200

    fixup! Implement SAN and FEN notations (and more...)
    This fixes commit 6354982dd6d75bcd384b7cb7c8f7eab756c77297.

--- src/notation/san.rs
+++ src/notation/san.rs
@@ -10,16 +10,11 @@ impl Square {
         ensure_ascii!(s);
         ensure_len!(s, 2);
 
-        // We use `as_bytes()` instead of `.chars()` because chess square notation
-        // is guaranteed to be ASCII ("a1"–"h8"). This avoids UTF-8 decoding,
-        // allocations, and iterator overhead, giving direct O(1) byte access.
         let bytes = s.as_bytes();
-
         let file = match bytes[0] {
             b'a'..=b'h' => File::try_from(ascii_delta_unchecked(bytes[0], b'a'))?,
             _ => return Err(anyhow!("invalid file")),
         };
-
         let rank = match bytes[1] {
             b'1'..=b'8' => Rank::try_from(ascii_delta_unchecked(bytes[1], b'1'))?,
             _ => return Err(anyhow!("invalid rank")),
